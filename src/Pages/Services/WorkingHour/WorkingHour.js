f2af9ddee6f32b6d58b945c127563851b2e0521b
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2021-10-18 21:05:44 +0600

    working hour addedd

--- src/Pages/Services/WorkingHour/WorkingHour.js
+++ src/Pages/Services/WorkingHour/WorkingHour.js
@@ -1,9 +1,56 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import "./WorkingHour.css";
 
 const WorkingHour = () => {
   return (
-    <div>
-      <h2>Working Hours</h2>
+    <div className="working-hour-container py-3">
+      <h2 className="title pt-3">Working Hours</h2>
+      <div className="underline mx-auto"></div>
+      <Container>
+        <div className="row g-4 mt-3">
+          <div className="col-12  col-lg-4">
+            <div className="d-flex justify-content-center">
+              <div className="me-5">
+                <div className="day mt-5">SUN</div>
+                <p className="subtitle ">Closed</p>
+              </div>
+              <div>
+                <div className="day">MON</div>
+                <p className="subtitle ">09:00-18:00</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12  col-lg-4">
+            <div className="d-flex justify-content-center">
+              <div className="me-5">
+                <div className="day">TUE</div>
+                <p className="subtitle ">09:00-18:00</p>
+              </div>
+              <div className="me-5">
+                <div className="day mt-5">WED</div>
+                <p className="subtitle ">09:00-18:00</p>
+              </div>
+              <div>
+                <div className="day">THU</div>
+                <p className="subtitle ">09:00-18:00</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12  col-lg-4">
+            <div className="d-flex justify-content-center">
+              <div className="me-5">
+                <div className="day">FRI</div>
+                <p className="subtitle ">09:00-18:00</p>
+              </div>
+              <div className="me-5">
+                <div className="day mt-5">SAT</div>
+                <p className="subtitle ">Closed</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
