daada473b77f24f2bc7f13a7c1a095849e0b7581
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2021-10-19 21:03:11 +0600

    not found addedd

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,15 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 import notFound from "../../images/404.png";
+import "./NotFound.css";
 
 const NotFound = () => {
   return (
-    <div className="padding-top">
-      <div>
-        <img
-          src={notFound}
-          style={{ width: "200px", height: "200px" }}
-          alt=""
-        />
+    <div className="padding-top not-found-container">
+      <div className="mx-auto error-content">
+        <div>
+          <img src={notFound} className="notfoundimag" alt="" />
+          <h2 className="title">OOPS...!! Page Not Found</h2>
+          <p className="subtitle">
+            Seems like you have assembled the page incorrectly
+          </p>
+          <Link to="/">
+            <Button>Back To Home</Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
